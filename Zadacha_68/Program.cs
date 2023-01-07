d57c13bf3d00a215443a38f22f083e85abb7f140
Yana <wyemelt@gmail.com>
2023-01-07 21:03:17 +0300

    Zadacha_68 done

---
+++ Zadacha_68/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.WriteLine($"Введите число M:");
+uint M = uint.Parse(Console.ReadLine());
+
+Console.WriteLine($"Введите число N:");
+uint N = uint.Parse(Console.ReadLine());
+
+static uint A(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+Console.WriteLine(A(M,N));
