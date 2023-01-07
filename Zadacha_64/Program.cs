f8a195479ad53030ce25a91a29e3157332d0fd6a
Yana <wyemelt@gmail.com>
2023-01-07 20:39:52 +0300

    Zadacha_64 done

---
+++ Zadacha_64/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine($"Введите число N:");
+int N = int.Parse(Console.ReadLine());
+
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+    else return n - 1;
+}
+
+
+for (int i = N; i > 1; i = i - 1)
+{
+    Console.WriteLine(Factorial(i));
+}
+
